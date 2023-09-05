e0c716301b9d1e30d73b9c7a2494fb95e60cfd44
Anurag Gundappa <41514107+an-anurag@users.noreply.github.com>
2023-09-05 08:37:09 +0000

    Added assignment 1

---
+++ stack_exchange/questions.sql
@@ -0,0 +1,10 @@
+-- Load the given databse stack_excahgne_dump.sql
+-- write SQL queries to answer questions below
+
+/*
+   1. How many post were made each year ?
+   2. How many votes were made in each day of the week (Sunday, Monday, Tuesday, etc.) ?
+   3. List all comments created on September 19th, 2012
+   4. List all users under the age of 33, living in London
+
+*/
