1feac85e888bfa7234dbcee90a0128ccec40b671
Anurag Gundappa <41514107+an-anurag@users.noreply.github.com>
2023-10-23 17:22:48 +0530

    Create questions.sql

---
+++ assignment_6/questions.sql
@@ -0,0 +1,19 @@
+Write a query to find the top 5 students with the highest GPA (grade point average), assuming 'A' is worth 4 points, 'B' is worth 3 points, 'C' is worth 2 points, and 'D' is worth 1 point. Calculate the GPA for each student and sort them in descending order of GPA.
+
+Write a query to display the courses that have the highest enrollment (most students enrolled) and the number of students enrolled in each of these courses.
+
+Write a query to find the students who have enrolled in all the courses in the 'Computer Science' department. Display their full names.
+
+Write a query to find the department with the highest average GPA. Calculate the average GPA for each department and display the department name along with the average GPA.
+
+Write a query to find the students who have consistently received 'A' grades (no other grades) in all the courses they've enrolled in. Display their full names.
+
+Write a query to find the courses that have the highest percentage of 'A' grades (grades equal to or greater than 'A'). Display the course names and the percentage of 'A' grades in each course.
+
+Write a query to find the students who have taken at least one course in both the 'Computer Science' department and the 'Mathematics' department. Display their full names.
+
+Write a query to find the students who have taken a course with 'Prof. Wilson' as the instructor and a course with 'Prof. Adams' as the instructor. Display their full names.
+
+Write a query to find the students who have taken courses from at least two different departments. Display their full names.
+
+Write a query to find the students who have taken more courses than the average number of courses taken by all students. Display their full names and the number of courses they've taken.
